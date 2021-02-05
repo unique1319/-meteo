f2b1526c95567e7627996248f819da6290f53589
wrh <409350480@qq.com>
2021-02-05 15:13:17 +0800

    [M] 修改文档注释

--- src/main/java/com/wrh/meteo/StringUtil.java
+++ src/main/java/com/wrh/meteo/StringUtil.java
@@ -15,19 +15,21 @@ public class StringUtil {
     public final static String DOLLAR_DOLLAR_REGEX = "(?<=\\u0024\\{).*?(?=})"; // 正则匹配${}表达式
 
     /**
-     * @description : 根据正则匹配字符串
-     * @date : 2021/2/5 10:41
+     * 根据正则匹配字符串
+     *
+     * @param str   字符串
+     * @param regex 匹配的正则
+     * @return List<String> 匹配到的字符串集合
      */
-    public static List<String> getDollarModels(String content, String regex) {
+    public static List<String> getDollarModels(String str, String regex) {
         Pattern r = Pattern.compile(regex);
-        Matcher m = r.matcher(content);
+        Matcher m = r.matcher(str);
         List<String> matchStrs = new ArrayList<>();
         while (m.find()) {
             matchStrs.add(m.group());
         }
         return matchStrs;
     }
-
 
     public static void main(String[] args) {
         String str = "E:/data/${yyyy}/{MM}/${dd}/";
