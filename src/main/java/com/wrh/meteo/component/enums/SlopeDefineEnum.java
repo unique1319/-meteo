3f5f98e00c152ffbce1205ab6a481c118fadd855
wrh <409350480@qq.com>
2021-02-24 13:57:21 +0800

    [A] 添加坡度定义枚举

---
+++ src/main/java/com/wrh/meteo/component/enums/SlopeDefineEnum.java
@@ -0,0 +1,25 @@
+package com.wrh.meteo.component.enums;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * @author wrh
+ * @version 1.0
+ * @date 2021/2/24 13:52
+ * @describe 坡度定义枚举类
+ */
+@AllArgsConstructor
+public enum SlopeDefineEnum {
+
+    PY("平原", 0.0, 0.5),
+    WXP("微斜坡", 0.5, 2.0),
+    HXP("缓斜坡", 2.00, 5.0),
+    XP("斜坡", 5.0, 15.0),
+    DP("陡坡", 15.0, 35.0),
+    QP("峭坡", 35.0, 55.0),
+    CZB("垂直壁", 55.0, 90.0),
+    ;
+    private final String name;
+    private final Double low;
+    private final Double upper;
+}
---
+++ src/main/java/com/wrh/meteo/draw/DrawImg.java
@@ -0,0 +1,21 @@
+package com.wrh.meteo.draw;
+
+import com.hxgis.meteodata.comdata.GridData;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * @author wrh
+ * @version 1.0
+ * @date 2021/2/24 13:47
+ * @describe
+ */
+public class DrawImg {
+
+    public BufferedImage drawGridData(GridData grid) {
+
+
+        return null;
+    }
+
+}
