38682228904656eada285040e13f0a32b7776db2
wrh <409350480@qq.com>
2021-02-25 17:36:47 +0800

    [M]

---
+++ src/main/java/com/wrh/meteo/util/DateUtil.java
@@ -0,0 +1,15 @@
+package com.wrh.meteo.util;
+
+/**
+ * @author wrh
+ * @version 1.0
+ * @date 2021/2/25 17:19
+ * @describe
+ */
+public class DateUtil {
+
+    public static final String YM_FMT = "yyyyMM";
+    public static final String YMD_FMT = "yyyyMMdd";
+    public static final String YMDH_FMT = "yyyyMMddHH";
+
+}
