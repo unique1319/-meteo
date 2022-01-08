690db3a193d108488d1ed08e88416ae5e556ae3b
wrh <wrh1319@163.com>
2022-01-08 14:01:25 +0800

    [A] 添加格点计算方法

--- src/main/java/com/wrh/meteo/util/grid/GridDataCalcUtil.java
+++ src/main/java/com/wrh/meteo/util/grid/GridDataCalcUtil.java
@@ -1,11 +1,15 @@
 package com.wrh.meteo.util.grid;
 
 import com.hxgis.meteodata.comdata.GridData;
+import com.wrh.meteo.util.NumberUtil;
 import org.springframework.lang.NonNull;
 
 import java.util.Arrays;
+import java.util.OptionalDouble;
 
 public class GridDataCalcUtil {
+
+    public static float INVALID_VAL = -999999f;
 
     public static GridData sum(@NonNull GridData... gridDatas) {
         return gridDataCalc(GridCalc::gridDataSum, gridDatas);
@@ -37,25 +41,28 @@ public class GridDataCalcUtil {
         return gridData;
     }
 
-
     @FunctionalInterface
     public interface GridCalc {
         float calc(int i, int j, GridData... gridDatas);
 
         static float gridDataSum(int i, int j, GridData... gridDatas) {
-            return (float) Arrays.stream(gridDatas).mapToDouble(g -> g.getGrid()[i][j]).sum();
+            float result = (float) Arrays.stream(gridDatas).mapToDouble(g -> g.getGrid()[i][j]).filter(v -> Math.abs(v) < Math.abs(INVALID_VAL)).sum();
+            return (float) NumberUtil.round(result, 1);
         }
 
         static float gridDataAvg(int i, int j, GridData... gridDatas) {
-            return (float) Arrays.stream(gridDatas).mapToDouble(g -> g.getGrid()[i][j]).average().getAsDouble();
+            OptionalDouble average = Arrays.stream(gridDatas).mapToDouble(g -> g.getGrid()[i][j]).filter(v -> Math.abs(v) < Math.abs(INVALID_VAL)).average();
+            return average.isPresent() ? (float) NumberUtil.round(average.getAsDouble(), 1) : INVALID_VAL;
         }
 
         static float gridDataMax(int i, int j, GridData... gridDatas) {
-            return (float) Arrays.stream(gridDatas).mapToDouble(g -> g.getGrid()[i][j]).max().getAsDouble();
+            OptionalDouble max = Arrays.stream(gridDatas).mapToDouble(g -> g.getGrid()[i][j]).filter(v -> Math.abs(v) < Math.abs(INVALID_VAL)).max();
+            return max.isPresent() ? (float) NumberUtil.round(max.getAsDouble(), 1) : INVALID_VAL;
         }
 
         static float gridDataMin(int i, int j, GridData... gridDatas) {
-            return (float) Arrays.stream(gridDatas).mapToDouble(g -> g.getGrid()[i][j]).min().getAsDouble();
+            OptionalDouble min = Arrays.stream(gridDatas).mapToDouble(g -> g.getGrid()[i][j]).filter(v -> Math.abs(v) < Math.abs(INVALID_VAL)).min();
+            return min.isPresent() ? (float) NumberUtil.round(min.getAsDouble(), 1) : INVALID_VAL;
         }
     }
 }
